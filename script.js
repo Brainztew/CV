1bec0900c3ebe28e8f44e0d79644f2caca4e28d8
dennis <isacsson_89@hotmail.com>
2024-01-25 16:03:38 +0100

    CV_update

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 console.log("Hej från JS");
 
  fetch("https://api.github.com/users/brainztew/repos")
-.then(function(response){
+ .then(function(response) {
     return response.json();
-})
-.then(function(json) {
-    console.log("github API", json);
-    
-     var githubrepos = document.getElementById("githubrepos");
+  })
+  .then(function(json) {
+    json.sort(function(a, b) {
+      return new Date(a.created_at) - new Date(b.created_at);
+    });
 
-    for(i=0; i<json.length; i++) {
-        githubrepos.insertAdjacentHTML("beforeEnd", "<a href='" + json[i].html_url + "' target='_blank'><div class'githubrepo'>" + json[i].name + "</div></a>");
-    }  
-}); 
+    console.log("github API", json);
+
+    var githubrepos = document.getElementById("githubrepos");
+
+    for (var i = 0; i < json.length; i++) {
+      githubrepos.insertAdjacentHTML(
+        "beforeEnd",
+        "<a href='" +
+          json[i].html_url +
+          "' target='_blank' class='githubrepo-link'><div class='githubrepo'>" +
+          json[i].name +
+          "</div></a>"
+      );
+    }
+  });
+ 
